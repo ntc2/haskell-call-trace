37a1af982e8a4e5533901ffa8907e48bc2069f8c
Nathan Collins <nathan.collins@gmail.com>
2013-12-21 15:41:22 -0800

    Add notes on printing tree-command style fancy ASCII trees.

---
+++ src/Debug/Trace/LogTree/Process/Tree.hs
@@ -0,0 +1,12 @@
+
+-- Fancy line drawing characters copied from the 'tree' program. File
+-- color.c in version 1.6.0, from
+-- http://mama.indstate.edu/users/ice/tree/. I believe I can replace
+-- the "\302\240" by spaces in the first command, and can probably
+-- embed the fancy chars directly in the haskell program.  Use 'print'
+-- in the shell to convert.
+{-
+{ utf8,        "\342\224\202\302\240\302\240",
+    "\342\224\234\342\224\200\342\224\200", "\342\224\224\342\224\200\342\224\200", "\302\251" },
+{ viscii,      "|  ",              "|--",            "`--",            "\371"     },
+-}
