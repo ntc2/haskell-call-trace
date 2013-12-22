840596d880087a93eb207d88e6ad9ac52e623284
Nathan Collins <nathan.collins@gmail.com>
2013-12-21 20:23:41 -0800

    Add most of a simple logger.
    From the comments:
    
      ----------------------------------------------------------------
      -- An event logger with a simple interface.
      --
      -- Specify the function you want to trace, along with a 'Symbol' to
      -- uniquely tag it (not implemented here), and get back a tracer for
      -- your function.  The pattern for lightweight logging is then
      --
      --   f :: t
      --   f = e
      --
      -- becomes
      --
      --   f , f' :: t
      --   f = simpleLogger "f" f'
      --   f' = e
      --
      -- creating a knot, where recursive calls in 'e' are still to 'f' and
      -- hence trigger recursive logging.
    
    The handling of 'Symbols' and related creation of 'SimpleCall'
    signatures is not implemented yet, but that part should be relatively
    simple. Here we have the implementation of a "collecting" call that
    wraps a function so that it's accumulated (uncurried) parameters get
    returned with it's result.  E.g. a function of type
    
      a1 -> ... -> an -> m r
    
    turns into a function of type
    
      a1 -> ... -> an -> ((a1 , (...(an , ()))) , m r)
    
    internally, and then the accumulation of arguments
    
      (a1 , (...(an , ())))
    
    can be used to construct a 'BeginCall' log event.
    
    The current version combines the accumulation with the logging, but it
    might be nicer to abstract out the pattern into a separate collection
    function that's used by a simpler logging function.  However, that
    might complicate the types even more ... we'll see.

---
+++ src/Debug/Trace/LogTree/SimpleLogger.hs
@@ -0,0 +1,95 @@
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
+module Debug.Trace.LogTree.SimpleLogger where
+
+import Control.Monad.Identity
+
+import Debug.Trace.LogTree.SimpleCall
+
+----------------------------------------------------------------
+-- An event logger with a simple interface.
+--
+-- Specify the function you want to trace, along with a 'Symbol' to
+-- uniquely tag it (not implemented here), and get back a tracer for
+-- your function.  The pattern for lightweight logging is then
+--
+--   f :: t
+--   f = e
+--
+-- becomes
+--
+--   f , f' :: t
+--   f = simpleLogger "f" f'
+--   f' = e
+--
+-- creating a knot, where recursive calls in 'e' are still to 'f' and
+-- hence trigger recursive logging.
+
+-- XXX: The 'tagged' package on hackage provides 'Proxy' and 'Tagged'
+-- types with some operations.
+data Proxy t = Proxy
+
+logB :: UncurryM t => Proxy t -> GetArg t -> GetMonad t ()
+logB = undefined
+
+logE :: UncurryM t => Proxy t -> GetRet t -> GetMonad t ()
+logE = undefined
+
+simpleLogger :: forall t. Collect t t => t -> t
+simpleLogger = simpleLoggerHelper (Proxy::Proxy t) id
+
+class (UncurryM b , UncurryM t) => Collect b t where
+  simpleLoggerHelper :: Proxy t -> (GetArg b -> GetArg t) -> b -> b
+
+-- BUG?: GHC doesn't like it when I lift a particular set of
+-- constraints and give them a name:
+{-
+{-# LANGUAGE ConstraintKinds #-}
+
+type Suffix b t = (UncurryM b , UncurryM t , GetRet b ~ GetRet t , GetMonad b ~ GetMonad t)
+
+instance Suffix (Identity r) t => Collect (Identity r) t where
+  simpleLoggerHelper p acc idr = do
+    logB p (acc ())
+    r <- idr
+    logE p r
+    return r
+-}
+-- generates:
+{-
+Debug/Trace/LogTree/SimpleLogger.hs:55:10:
+    Variable s `r, t, r, t' occur more often than in the instance head
+      in the constraint: Suffix (Identity r) t
+    (Use -XUndecidableInstances to permit this)
+    In the instance declaration for `Collect (Identity r) t'
+Failed, modules loaded: Debug.Trace.LogTree, Debug.Trace.LogTree.SimpleCall.
+-}
+-- whereas the expanded versions below are fine.  Of course, the
+-- versions below are only "smaller" because the definitions of the
+-- type functions 'GetRet' and 'GetMonad' have been expanded.
+
+instance (UncurryM t , GetRet t ~ r , GetMonad t ~ Identity) => Collect (Identity r) t where
+  simpleLoggerHelper p acc idr = do
+    logB p (acc ())
+    r <- idr
+    logE p r
+    return r
+
+instance (UncurryM t , GetRet t ~ r , GetMonad t ~ (trans m) , Monad (trans m) , Monad m)
+      => Collect (trans m r) t where
+  simpleLoggerHelper p acc tmr = do
+    logB p (acc ())
+    r <- tmr
+    logE p r
+    return r
+
+instance (UncurryM t , UncurryM b , Collect b t , GetRet t ~ GetRet b , GetMonad t ~ GetMonad b)
+      => Collect (a -> b) t where
+  simpleLoggerHelper p acc f x =
+    simpleLoggerHelper p (\xs -> acc (x , xs)) (f x)
+
+----------------------------------------------------------------
