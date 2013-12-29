1f3e3ee2caa8462310bbb8d7c44d53a12b049ca2
Nathan Collins <nathan.collins@gmail.com>
2013-12-29 06:29:21 -0800

    Improve proof-tree helper script and add tests to make file.

--- proof-trees/tm2latex.sh
+++ proof-trees/tm2latex.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
-# usage: $0 MODE TERM
+if (($# != 3)); then
+  echo "usage: $0 OUT CTX TERM"                                               > /dev/stderr
+  echo ""                                                                     > /dev/stderr
+  echo "Run ./tmp/proof-tree MODE CTX TERM > OUT.MODE.tex for both modes and" > /dev/stderr
+  echo "process to PDF."                                                      > /dev/stderr
+  exit 2
+fi
 
-make && ./tmp/proof-tree "$1" "$2" > tmp/proof.tex && make tmp/proof.pdf 
+out="$1"
+ctx="$2"
+tm="$3"
+
+for mode in True False; do
+  ./tmp/proof-tree "$mode" "$ctx" "$tm" > tmp/"$out"."$mode".tex && \
+  make tmp/"$out"."$mode".pdf
+done
