c672d4b87d284df777d7526bae844a336bb6f69e
Nathan Collins <nathan.collins@gmail.com>
2014-01-15 14:49:52 -0800

    Improve 'UnixTree' output when body and footer are empty.
    Now, when the body and footer are empty, the last line of the header
    is formatted specially.  The case of a single-line header is handled
    specially.
    
    Examples
    ========
    
    These are three different 'UnixTree' formattings of the same trace.
    
    Single-line headers:
    
      ┬ <()> h (Just 6) = <error: there>
      └─┬ <()> f 6 = <error: there>
        └─┬ <()> f 5 = <error: here>
          └─┬ <()> f 2 = "XX Y" <()>
            ├─┬ <()> f 1 = "X Y" <()>
            │ ├── <()> f 0 = " Y" <()>
            │ └── <()> g "X" " Y" = "X Y" <()>
            └── <()> g "X" "X Y" = "XX Y" <()>
    
    Multi-line headers:
    
      ┬ ()
      ├ h (Just 6) = <error: there>
      └─┬ ()
        ├ f 6 = <error: there>
        └─┬ ()
          ├ f 5 = <error: here>
          └─┬ ()
            ├ f 2 = "XX Y"
            ├ ()
            ├─┬ ()
            │ ├ f 1 = "X Y"
            │ ├ ()
            │ ├─┬ ()
            │ │ ├ f 0 = " Y"
            │ │ └ ()
            │ └─┬ ()
            │   ├ g "X" " Y" = "X Y"
            │   └ ()
            └─┬ ()
              ├ g "X" "X Y" = "XX Y"
              └ ()
    
    Headers and footers (this style is unchanged by this commit):
    
      ┬ ()
      ├ h (Just 6)
      ├─┬ ()
      │ ├ f 6
      │ ├─┬ ()
      │ │ ├ f 5
      │ │ ├─┬ ()
      │ │ │ ├ f 2
      │ │ │ ├─┬ ()
      │ │ │ │ ├ f 1
      │ │ │ │ ├─┬ ()
      │ │ │ │ │ ├ f 0
      │ │ │ │ │ ╞ " Y"
      │ │ │ │ │ ╘ ()
      │ │ │ │ ├─┬ ()
      │ │ │ │ │ ├ g "X" " Y"
      │ │ │ │ │ ╞ "X Y"
      │ │ │ │ │ ╘ ()
      │ │ │ │ ╞ "X Y"
      │ │ │ │ ╘ ()
      │ │ │ ├─┬ ()
      │ │ │ │ ├ g "X" "X Y"
      │ │ │ │ ╞ "XX Y"
      │ │ │ │ ╘ ()
      │ │ │ ╞ "XX Y"
      │ │ │ ╘ ()
      │ │ ╘ <error: here>
      │ ╘ <error: there>
      ╘ <error: there>

--- src/Debug/Trace/LogTree/Process/UnixTree.hs
+++ src/Debug/Trace/LogTree/Process/UnixTree.hs
@@ -38,41 +38,60 @@ unixTree (Ex2T t@(CallAndError {})) =
                           maybe [] (:[]) (_how t))
 
 layer' :: [String] -> [[String]] -> [String] -> [String]
-layer' = layer ("┬ " , "├ ") ("├─" , "│ " , "└─" , "  ") ("╞ " , "╘ ")
+layer' = layer ("─ " , "┬ " , "├ " , "└ ")
+
+               ("├─" , "│ " , "└─" , "  ")
+
+               ("╞ " , "╘ ")
 
 ----------------------------------------------------------------
 
--- Prefix first line with 'first' and rest of lines with 'rest'.
-prefix :: String -> String -> [String] -> [String]
-prefix _ _ [] = []
-prefix first rest (l:ls) = (first ++ l) : map (rest ++) ls
-
--- Like 'prefix', but instead initial and last line are distinguished.
-prefix' :: String -> String -> [String] -> [String]
-prefix' init last = reverse . prefix last init . reverse
+-- Prefix first line with 'first', intermediate lines with 'between',
+-- and last line with 'last''. If there is only one line, then prefix
+-- with 'firstAndOnly' instead.
+prefix :: String -> String -> String -> String -> [String] -> [String]
+prefix _ _ _ _ [] = []
+prefix firstAndOnly _ _ _ [l] = [firstAndOnly ++ l]
+prefix _ first between last' (l:ls) =
+  [first ++ l] ++
+  map (between ++) (init ls) ++
+  [last' ++ last ls]
 
 -- Layer a header 'hs', body 'bss', and footer 'fs'.
 --
 -- The 'bLastFirst' and 'bLastRest' are used only if the footer is
--- empty; otherwise, 'bFirst' and 'bRest' are used for all body lines.
+-- empty; otherwise, 'bFirst' and 'bRest' are used for all body
+-- lines.
+--
+-- The 'hFirstAndNoMore' and 'hLastAndNoMore' are used when the body
+-- and footer are empty: the 'hFirstAndNoMore' is used when the header
+-- consists of a single line, and the 'hLastAndNoMore' is used for the
+-- last line of the header otherwise.
 --
 -- Assumes the header is non-empty.
-layer :: (String , String) -> (String , String , String , String) -> (String , String) ->
+layer :: (String , String , String , String) ->
+         (String , String , String , String) ->
+         (String , String) ->
          [String] -> [[String]] -> [String] -> [String]
-layer (hFirst , hRest) (bFirst , bRest , bLastFirst , bLastRest) (fInit , fLast) hs bss fs =
+layer (hFirstAndNoMore , hFirst , hRest , hLastAndNoMore)
+      (bFirst , bRest , bLastFirst , bLastRest)
+      (fInit , fLast) hs bss fs =
   hs' ++ concat bss' ++ fs'
   where
-    hs' = assert (not . null $ hs) $ prefix hFirst hRest hs
-    fs' = prefix' fInit fLast fs
+    hs' = assert (not . null $ hs) $
+      if null fs && null bss
+      then prefix hFirstAndNoMore hFirst hRest hLastAndNoMore hs
+      else prefix hFirst          hFirst hRest hRest          hs
+    fs' = prefix fLast fInit fInit fLast fs
     bss' = case bss of
       [] -> []
       _  ->
         if null fs
         -- Empty footer: prefix last group of body lines specially.
-        then map (prefix bFirst bRest) (init bss) ++
-             [prefix bLastFirst bLastRest (last bss)]
+        then map (prefix bFirst bFirst bRest bRest) (init bss) ++
+             [prefix bLastFirst bLastFirst bLastRest bLastRest (last bss)]
         -- Non-empty footer: prefix all body groups equally.
-        else map (prefix bFirst bRest) bss
+        else map (prefix bFirst bFirst bRest bRest) bss
 
 ----------------------------------------------------------------
 
