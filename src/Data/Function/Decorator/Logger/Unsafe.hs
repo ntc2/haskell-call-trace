d9bb1453d134cf88394eed5056f5f6284a6856c5
Nathan Collins <nathan.collins@gmail.com>
2014-04-28 21:05:18 -0700

    Add 'unsafeTrace', a version of 'trace' for pure functions.
    The pure function 'f' in 'unsafeTrace f' still needs to be open
    recursive or mutual recursive, so that we can get inside the knot, but
    by using 'unsafePerformIO' we can lift the need for a monad.
    
    I don't completely understand why I need to force the return value in
    'trace'; I don't need to in the "safe" version.

---
+++ src/Data/Function/Decorator/Logger/Unsafe.hs
@@ -0,0 +1,62 @@
+-- http://hackage.haskell.org/package/base-4.7.0.0/docs/System-IO-Unsafe.html#v:unsafePerformIO
+{-# OPTIONS_GHC -fno-cse -fno-full-laziness #-}
+
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE GADTs #-}
+
+module Data.Function.Decorator.Logger.Unsafe where
+
+import Prelude hiding (curry , uncurry)
+
+import Data.IORef
+import Data.Proxy
+import System.IO.Unsafe
+
+import Data.Function.Decorator.Curry
+import Data.Function.Decorator.Logger.HetCall
+import Data.Function.Decorator.Logger.Logger
+
+----------------------------------------------------------------
+
+{-# NOINLINE globalIndentLevel #-}
+globalIndentLevel :: IORef Int
+globalIndentLevel = unsafePerformIO $ newIORef 0
+
+-- This is a special case of a general pattern: make a unsafe version
+-- of a safe function by wrapping in 'unsafePeformBlah'.
+
+{-# NOINLINE unsafeTrace #-}
+unsafeTrace :: forall n t.
+  ( Uncurry n t
+  , CurriedUncurried n t ~ t
+-- XXX: introduce a synonym for this long type:
+--
+--  , GetArg n t `Curried` IO (GetRet n t) ~ foo
+--
+-- or even better: introduce one big synonym for all of these
+-- constraints!
+  , GetArg n (GetArg n t `Curried` IO (GetRet n t)) ~ GetArg n t
+  , GetRet n (GetArg n t `Curried` IO (GetRet n t)) ~ IO (GetRet n t)
+
+  , GetArgM    (GetArg n t `Curried` IO (GetRet n t)) ~ GetArg n t
+  , GetRetM    (GetArg n t `Curried` IO (GetRet n t)) ~ GetRet n t
+  , GetMonad  (GetArg n t `Curried` IO (GetRet n t)) ~ IO
+
+  , Uncurry n (GetArg n t `Curried` IO (GetRet n t))
+  , UncurryM  (GetArg n t `Curried` IO (GetRet n t))
+
+  , HFold Show (GetArg n t)
+  , Show (GetRet n t)
+  ) => Proxy n -> String -> t -> t
+unsafeTrace p name f =
+  compose p unsafePerformIO' .
+  trace (return globalIndentLevel) name $
+  compose p return' f
+  where
+    -- Polymorphism confuses 'compose'.
+    return'          :: GetRet n t -> IO (GetRet n t)
+    unsafePerformIO' :: IO (GetRet n t) -> GetRet n t
+    return'          = return
+    unsafePerformIO' = unsafePerformIO
